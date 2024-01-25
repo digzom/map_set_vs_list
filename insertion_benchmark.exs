54eb54cdc2d08cb4e5be225e0f87240cb761b6a0
Dickson <dickson.melo@idopterlabs.com.br>
2024-01-25 20:09:09 -0300

    refactor: doing it on a not dumb way

--- insertion_benchmark.exs
+++ insertion_benchmark.exs
@@ -1,26 +1,41 @@
 Mix.install([:jason, :benchee])
 
 defmodule ListInsertion do
-  def insert_band(band) do
-    bands_list = File.read!("bands.json") |> Jason.decode!()
-    List.insert_at(bands_list, 0, band)
+  @bands_list File.read!("bands.json") |> Jason.decode!()
+
+  def insert_band_first() do
+    Enum.reduce(1..500, @bands_list, fn band, acc ->
+      ["band-#{band}" | acc]
+    end)
+  end
+
+  def insert_band_last() do
+    Enum.reduce(1..500, @bands_list, fn band, acc ->
+      acc ++ ["band-#{band}"]
+    end)
   end
 end
 
 defmodule MapSetInsertion do
-  def insert_band(band) do
-    bands_set = File.read!("bands.json") |> Jason.decode!() |> MapSet.new()
-    MapSet.put(bands_set, band)
+  @bands_set File.read!("bands.json") |> Jason.decode!() |> MapSet.new()
+
+  def insert_band() do
+    Enum.reduce(1..500, @bands_set, fn band, acc ->
+      MapSet.put(acc, "band-#{band}")
+    end)
   end
 end
 
 Benchee.run(
   %{
-    "list" => fn ->
-      _result = ListInsertion.insert_band("New Band")
+    "list first" => fn ->
+      _result = ListInsertion.insert_band_first()
+    end,
+    "list last" => fn ->
+      _result = ListInsertion.insert_band_last()
     end,
     "MapSet" => fn ->
-      _result = MapSetInsertion.insert_band("New Band")
+      _result = MapSetInsertion.insert_band()
     end
   },
   time: 10,
